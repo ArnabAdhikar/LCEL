e02f37fee9edb61ffb4055c8241cc0da7e79b4f0
Arnab Adhikary <100219485+ArnabAdhikar@users.noreply.github.com>
2025-12-03 21:01:02 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,26 @@
+# First Run the serve.py file then run this file
+
+import requests
+import streamlit as st
+
+def get_groq_response(input_text):
+    json_body={
+      "input": {
+        "language": "Bengali",
+        "text": input_text
+      },
+      "config": {},
+      "kwargs": {}
+    }
+    response=requests.post("http://127.0.0.1:8000/chain/invoke", json=json_body)
+
+    print(response.json())
+
+    return response.json()
+
+# Streamlit app
+st.title("LLM Application Using LCEL")
+input_text=st.text_input("Enter the text you want to convert to Bengali")
+
+if input_text:
+    st.write(get_groq_response(input_text))
